e7e155131fc0e86116f0fbd55fff3d474f2583c1
delwar36 <delwarh543@gmail.com>
2021-02-05 15:38:25 +0600

    new commit

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(compression())
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
@@ -26,4 +27,4 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(3000)
+server.listen(process.env.PORT || 3000)
