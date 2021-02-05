1bc42eba3ae7fed35942eba651bc1cd443955759
delwar36 <delwarh543@gmail.com>
2021-02-05 15:42:50 +0600

    posrt changed

--- public/script.js
+++ public/script.js
@@ -1,6 +1,10 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer()
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: '3001'
+}
+)
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
